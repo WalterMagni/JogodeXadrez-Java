a56eb54dbbdb98b055fa716f7acff3d6daf5337a
WalterMagni <waltermagni@terra.com.br>
2018-08-27 12:01:51 -0300

    BoardException e programação Defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -16,6 +16,10 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int colums) {
+        if (rows < 1 || colums < 1) {
+            throw new BoardException("Erro ao criar a borda: deve ser no minimo 1 linha e 1 coluna");
+        }
+        
         this.rows = rows;
         this.colums = colums;
         pieces = new Piece[rows][colums];
@@ -25,29 +29,44 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColums() {
         return colums;
     }
 
-    public void setColums(int colums) {
-        this.colums = colums;
-    }
-    
     public Piece piece(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Posição não esta na borda");
+        }
         return pieces[row][column];
     }
     
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Posição não esta na borda");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
     
     public void placePiece(Piece piece, Position position) {
+        if (thereIsaPiece(position)) {
+            throw new BoardException("Já existe uma peça na posição "+ position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
     
+    private boolean positionExists(int row, int column){
+       return row >= 0 && row < rows && column >= 0 && column < colums;       
+    }
+    
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    
+    public boolean thereIsaPiece(Position position) {
+       if (!positionExists(position)) {
+            throw new BoardException("Posição não esta na borda");
+       } 
+       return piece(position) != null;
+    }
 }
